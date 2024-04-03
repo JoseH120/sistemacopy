27d011f2ff3eedafe5f4b8ad0534774eb5cb28bf
Salvador Gonzalez <salvador_ant@outlook.com>
2024-04-02 20:58:26 -0600

    CRUD Tutores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,20 @@ $routes->group('api', ['namespace' => 'App\Controllers\API'], function($routes){
    //http://localhost/sistema/api/estudiantes
    $routes->get('estudiantes', 'Estudiantes::index'); 
 
+
    //http://localhost/sistema/api/tutores
    $routes->get('tutores', 'Tutores::index'); 
+
+   //http://localhost/sistema/api/tutores/create
+   $routes->post('tutores/create', 'Tutores::create'); 
+
+   //http://localhost/sistema/api/tutores/edit
+   $routes->get('tutores/edit/(:num)', 'Tutores::edit/$1');
+
+   //http://localhost/sistema/api/tutores/update
+   $routes->put('tutores/update/(:num)', 'Tutores::update/$1');
+
+   //http://localhost/sistema/api/tutores/delete
+   $routes->delete('tutores/delete/(:num)', 'Tutores::delete/$1');
+
 });
--- app/Controllers/API/Tutores.php
+++ app/Controllers/API/Tutores.php
@@ -3,6 +3,7 @@
 
     use App\Models\TutoresModel;
     use CodeIgniter\RESTful\ResourceController;
+    use Exception;
 
     class Tutores extends ResourceController{
 
@@ -10,10 +11,88 @@
         {
             $this->model = $this->setModel(new TutoresModel());
         }
-
+        //Listar 
         public function index(){
             $tutores = $this->model->findAll();
             return $this->respond($tutores);
+        }
+
+        //servicio de insertar
+        public function create (){
+            try{
+                $tutores = $this->request->getJSON();
+                if($this->model->insert($tutores)){
+                    $tutores->IdTutor = $this->model->insertID();
+                    return $this->respondCreated($tutores);
+                }
+                else{
+                    return $this->failValidationError($this->model->listErrors());
+                }
+            }
+            catch(Exception $e){
+                return $this->failServerError('Ha ocurrido un error en el servidor.');
+            }
+        }
+
+        //Servicio para buscar un registro
+        public function edit($id = null){
+            try {
+                if($id == null){
+                    return $this->failValidationError('No se ha enviado un id valido');
+                }
+                $tutor = $this->model->find($id);
+                if($tutor == null){
+                    return $this->failNotFound('No se ha encorntrado un registro con el ID: '.$id. ' enviado');
+                }
+                return $this->respond($tutor);
+            } catch (Exception $e) {
+                return $this->failServerError('Ha ocurrido un error en el servidor');
+            }
+        }
+
+        //Servicio de actualizar un registro
+        public function update($id = null){
+            try {
+                if($id == null){
+                    return $this->failValidationError('No se ha enviado un id valido');
+                }
+                $tutorVerificado = $this->model->find($id);
+                if($tutorVerificado == null){
+                    return $this->failNotFound('No se ha encorntrado un registro con el ID: '.$id. ' enviado');
+                }
+                $tutor = $this->request->getJSON();
+
+                if($this->model->update($id, $tutor)){
+                    $tutor->IdTutor = $id;
+                    return $this->respondUpdated($tutor);
+                }
+                else{
+                    return $this->failValidationError($this->model->validation->listErrors());
+                }
+            } catch (Exception $e) {
+                return $this->failServerError('Ha ocurrido un error en el servidor');
+            }
+        }
+
+        public function delete($id = null){
+            try {
+                if($id == null){
+                    return $this->failValidationError('No se ha enviado un id valido');
+                }
+                $tutorVerificado = $this->model->find($id);
+                if($tutorVerificado == null){
+                    return $this->failNotFound('No se ha encorntrado un registro con el ID: '.$id. ' enviado');
+                }
+                
+                if($this->model->delete($id)){
+                    return $this->respondDeleted($tutorVerificado);
+                }
+                else{
+                    return $this->failValidationError('No se ha podido eliminar el registro');
+                }
+            } catch (Exception $e) {
+                return $this->failServerError('Ha ocurrido un error en el servidor');
+            }
         }
     }
 ?>
