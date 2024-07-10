21545b0035f397f9a13e328e4ad6f89b2f10394b
Salvador Gonzalez <salvador_ant@outlook.com>
2024-07-10 15:34:59 -0600

    cambio de vacio a Notas

--- app/Models/ActividadesEstudiantesModel.php
+++ app/Models/ActividadesEstudiantesModel.php
@@ -7,19 +7,19 @@
         protected $table = 'actividades_estudiantes';
         protected $primaryKey = 'Idactividad_estudiante';
         protected $returnType = 'array';
-        protected $allowedFields = ['Nota', 'UrlTarea', 'IdActividad'];
+        protected $allowedFields = ['Nota', 'UrlTarea', 'IdActividad', 'IdEstudiante'];
         protected $useTimestamps = true;
         protected $createdField = 'created_at';
         protected $updatedField = 'updated_at';
         protected $validationRules = [
-            'Nota' => 'required|numeric|less_than[11]|greater_than[0]',
+            'Nota' => 'permit_empty|numeric|less_than[11]|greater_than[0]',
             'UrlTarea' => 'required|min_length[3]|max_length[600]',
-            'IdActividad' => 'required|numeric|is_valid_actividad'
+            'IdActividad' => 'required|numeric|is_valid_actividad',
+            'IdEstudiante' => 'required|numeric|is_valid_estudiante'
         ];
 
         protected $validationMessages = [
             'Nota' => [
-                'required' => 'El valor es requerido',
                 'numeric' => 'Debe ingresar un valor numerico',
                 'less_than' => 'El numero debe ser menor o igual a 10',
                 'greater_than' => 'El numero debe ser mayor o igual a 1'
@@ -33,6 +33,11 @@
                 'required' => 'El valor es requerido',
                 'numeric' => 'Debe ingresar un valor numerico',
                 'is_valid_actividad' => 'el id actividad no existe'
+            ],
+            'IdEstudiante' => [
+                'required' => 'El valor es requerido',
+                'numeric' => 'Debe ingresar un valor numerico',
+                'is_valid_actividad' => 'el id estudiante no existe'
             ]
 
         ];
