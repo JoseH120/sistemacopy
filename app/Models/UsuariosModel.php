9aecf97fe4c8aa81bdc6933505b4937c55aee9cd
Jose Hernandez <reyes_7374@outlook.es>
2024-09-16 13:38:51 -0600

    completando merge con repositorio remoto

--- app/Models/UsuariosModel.php
+++ app/Models/UsuariosModel.php
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-    namespace App\Models;
-
-    use CodeIgniter\Model;
-
-    class UsuariosModel extends Model{
-
-        //Tipo = [ÄDMINISTRADOR => 1, TUTOR => 2, ESTUDIANTE => 3]
-
-        protected $table = 'usuarios';
-        protected $primaryKey = 'IdUsuario';
-        protected $returnType = 'array';
-        protected $allowedFields = ['Usuario', 'Clave', 'Tipo'];
-        protected $useTimestamps = true;
-        protected $createdField = 'created_at';
-        protected $updatedField = 'updated_at';
-        protected $validationRules = [
-        "Usuario" => 'required|max_length[30]',
-        "Clave" => 'required|max_length[255]|min_length[10]',
-        "Tipo" => 'required',
-        ];
-        protected $validationMessages = [
-            'Usuario' => [
-                'required' => 'El valor "usuario" es requerido',
-                'max_length' => '"Usuario" debe tener 30 caracteres como maximo'
-            ],
-            'Clave' => [
-                'required' => 'El valor "contraseña" es requerido',     
-                'min_length' => '"Contraseña" debe contener al menos 8 caracteres',
-                'max_length' => '"Contraseña" debe contener menos que 255 caracteres'
-            ],
-            'Tipo' => [
-                'required' => 'El valor "tipo" es requerido'
-            ]
-        ];
-        protected $skipValidation = false;
-
-        public function usuariosNoAsignados(){
-            $builder = $this->db->table($this->table);
-            $builder->select('IdUsuario, Usuario, Tipo');
-            $builder->where('IdUsuario NOT IN (SELECT idusuario FROM tutores)');
-
-            $query = $builder->get();
-
-            return $query->getResult();
-        }
-    }
-
-
-=======
 <?php
 
 namespace App\Models;
@@ -119,4 +67,3 @@ class UsuariosModel extends Model
         return $data;
     }
 }
->>>>>>> loginfeature
