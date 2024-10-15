d09cc0cdf12a5adc7f1e5945bc09ef3fd1d8cf35
Jose Hernandez <reyes_7374@outlook.es>
2024-10-14 19:26:59 -0600

    modificando el archivo EstudiantModel.php en regla de validaciones

--- app/Models/EstudiantesModel.php
+++ app/Models/EstudiantesModel.php
@@ -1,111 +1,122 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use CodeIgniter\Model;
+use CodeIgniter\Model;
 
-    class EstudiantesModel extends Model{
-        
-        protected $table = 'estudiantes';
-        protected $primaryKey = 'IdEstudiante';
+class EstudiantesModel extends Model
+{
 
-        protected $returnType = 'array';
-        protected $allowedFields = ['PrimerNombre', 'SegundoNombre', 'PrimerApellido', 'SegundoApellido', 'Dui', 
-                                    'Direccion', 'FechaNacimiento', 'Responsable', 'Correo', 
-                                    'VacunaCovid', 'IdUsuario'];
+    protected $table = 'estudiantes';
+    protected $primaryKey = 'IdEstudiante';
 
-        protected $useTimestamps = true;
-        protected $createdField = 'created_at';
-        protected $updatedField = 'updated_at';
+    protected $returnType = 'array';
+    protected $allowedFields = [
+        'PrimerNombre',
+        'SegundoNombre',
+        'PrimerApellido',
+        'SegundoApellido',
+        'Dui',
+        'Direccion',
+        'FechaNacimiento',
+        'Responsable',
+        'Correo',
+        'VacunaCovid',
+        'IdUsuario'
+    ];
 
-        protected $validationRules = [
-            'PrimerNombre' => 'required|alpha|min_length[3]|max_length[500]',
-            'SegundoNombre' => 'required|alpha|min_length[3]|max_length[500]',
-            'PrimerApellido' => 'required|alpha|min_length[3]|max_length[500]',
-            'SegundoApellido' => 'required|alpha|min_length[3]|max_length[500]',
-            'Dui' => 'permit_empty|exact_length[9,10]',
-            'Direccion' => 'required|alpha_dash|min_length[3]|max_length[500]',
-            'FechaNacimiento' => 'required|valid_date',
-            'Responsable' => 'permit_empty|alpha_space|min_length[3]|max_length[500]',
-            'Correo' => 'permit_empty|valid_email',
-            'VacunaCovid' => 'required|numeric|greater_than[0]|less_than[3]',
-            'IdUsuario' => 'required|numeric|is_valid_usuario'
-        ];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
 
-        protected $validationMessages = [
-            'PrimerNombre' => [
-                'required' => 'El valor "primer nombre" es requerido',
-                'alpha' => 'El "primer nombre" solo debe contener letras del alfabeto',
-                'min_length' => '"Primer nombre" debe ser mayor que 3 caracteres',
-                'max_length' => '"Primer nombre" debe ser menor de 500 caracteres'
+    protected $validationRules = [
+        'PrimerNombre' => 'required|alpha|min_length[3]|max_length[500]',
+        'SegundoNombre' => 'required|alpha|min_length[3]|max_length[500]',
+        'PrimerApellido' => 'required|alpha|min_length[3]|max_length[500]',
+        'SegundoApellido' => 'required|alpha|min_length[3]|max_length[500]',
+        'Dui' => 'permit_empty|exact_length[9,10]',
+        'Direccion' => 'required|alpha_numeric_space|min_length[3]|max_length[500]',
+        'FechaNacimiento' => 'required|valid_date',
+        'Responsable' => 'permit_empty|alpha_space|min_length[3]|max_length[500]',
+        'Correo' => 'permit_empty|valid_email',
+        'VacunaCovid' => 'required|numeric|greater_than[0]|less_than[3]',
+        'IdUsuario' => 'required|numeric|is_valid_usuario'
+    ];
 
-            ],
-            'SegundoNombre' => [
-                'required' => 'El valor "segundo nombre" es requerido',
-                'alpha' => 'El "segundo nombre" solo debe contener letras del alfabeto',
-                'min_length' => '"Segundo nombre" debe ser mayor que 3 caracteres',
-                'max_length' => '"Segundo nombre" debe ser menor de 500 caracteres'
+    protected $validationMessages = [
+        'PrimerNombre' => [
+            'required' => 'El valor "primer nombre" es requerido',
+            'alpha' => 'El "primer nombre" solo debe contener letras del alfabeto',
+            'min_length' => '"Primer nombre" debe ser mayor que 3 caracteres',
+            'max_length' => '"Primer nombre" debe ser menor de 500 caracteres'
 
-            ],
-            'PrimerApellido' => [
-                'required' => 'El valor "primer apellido" es requerido',
-                'alpha' => 'El "primer apellido" solo debe contener letras del alfabeto',
-                'min_length' => '"Primer apellido" debe ser mayor que 3 caracteres',
-                'max_length' => '"Primer apellido" debe ser menor de 500 caracteres'
+        ],
+        'SegundoNombre' => [
+            'required' => 'El valor "segundo nombre" es requerido',
+            'alpha' => 'El "segundo nombre" solo debe contener letras del alfabeto',
+            'min_length' => '"Segundo nombre" debe ser mayor que 3 caracteres',
+            'max_length' => '"Segundo nombre" debe ser menor de 500 caracteres'
 
-            ],
-            'SegundoApellido' => [
-                'required' => 'El valor "segundo apellido" es requerido',
-                'alpha' => 'El "segundo apellido" solo debe contener letras del alfabeto',
-                'min_length' => '"Segundo apellido" debe ser mayor que 3 caracteres',
-                'max_length' => '"Segundo apellido" debe ser menor de 500 caracteres'
+        ],
+        'PrimerApellido' => [
+            'required' => 'El valor "primer apellido" es requerido',
+            'alpha' => 'El "primer apellido" solo debe contener letras del alfabeto',
+            'min_length' => '"Primer apellido" debe ser mayor que 3 caracteres',
+            'max_length' => '"Primer apellido" debe ser menor de 500 caracteres'
 
-            ],
-            'Dui' => [
-                'exact_length' => '"DUI" debe tener entre 9 a 10 caracteres para ser aceptado'
-            ],
-            'Direccion' => [
-                'required' => 'El valor "direccion" es requerido',
-                'alpha_dash' => '"Direccion" solo recibe caracteres alfanuméricos, guiones bajos o guiones en ASCII',
-                'min_length' => '"Direccion" debe ser mayor que 3 caracteres',
-                'max_length' => '"Direccion" debe ser menor de 500 caracteres'
-            ],
-            'FechaNacimiento' => [
-                'required' => 'El valor "fecha nacimiento" es requerido',
-                'valid_date' => 'La fecha es invalida',
-            ],
-            'Responsable' => [
-                'alpha_space' => 'El nombre del "responsable" solo debe contener letras del alfabeto y/o espacios',
-                'min_length' => 'El nombre del "responsable" debe ser mayor que 3 caracteres',
-                'max_length' => 'El nombre del "responsable" debe ser menor de 500 caracteres'
-            ],
-            'Correo' => [
-                'valid_email' => 'Estimado usuario, debe ingresar un email valido'
-            ],
-            'VacunaCovid' => [
-                'required' => 'El valor "vacuna COVID" es requerido',
-                'numeric'=> '"Vacuna COVID" debe ser un numero entre [1-2]',
-                'greater_than' => 'El numero "vacuna COVID" debe ser mayor que 0',
-                'less_than' => 'El numero "vacuna COVID" debe ser menor que 3',
-            ],
-            'IdUsuario' => [
-                'required' => 'El valor "ID usuario" es requerido',
-                'numeric'=> 'El "ID usuario" debe ser un numero',
-                'is_valid_usuario' => 'el id usuario no existe'
-            ]
-        ];
+        ],
+        'SegundoApellido' => [
+            'required' => 'El valor "segundo apellido" es requerido',
+            'alpha' => 'El "segundo apellido" solo debe contener letras del alfabeto',
+            'min_length' => '"Segundo apellido" debe ser mayor que 3 caracteres',
+            'max_length' => '"Segundo apellido" debe ser menor de 500 caracteres'
 
-        protected $skipValidation = false;
+        ],
+        'Dui' => [
+            'exact_length' => '"DUI" debe tener entre 9 a 10 caracteres para ser aceptado'
+        ],
+        'Direccion' => [
+            'required' => 'El valor "direccion" es requerido',
+            'alpha_numeric_space' => '"Direccion" solo recibe caracteres alfanuméricos, y espacios en ASCII',
+            'min_length' => '"Direccion" debe ser mayor que 3 caracteres',
+            'max_length' => '"Direccion" debe ser menor de 500 caracteres'
+        ],
+        'FechaNacimiento' => [
+            'required' => 'El valor "fecha nacimiento" es requerido',
+            'valid_date' => 'La fecha es invalida',
+        ],
+        'Responsable' => [
+            'alpha_space' => 'El nombre del "responsable" solo debe contener letras del alfabeto y/o espacios',
+            'min_length' => 'El nombre del "responsable" debe ser mayor que 3 caracteres',
+            'max_length' => 'El nombre del "responsable" debe ser menor de 500 caracteres'
+        ],
+        'Correo' => [
+            'valid_email' => 'Estimado usuario, debe ingresar un email valido'
+        ],
+        'VacunaCovid' => [
+            'required' => 'El valor "vacuna COVID" es requerido',
+            'numeric' => '"Vacuna COVID" debe ser un numero entre [1-2]',
+            'greater_than' => 'El numero "vacuna COVID" debe ser mayor que 0',
+            'less_than' => 'El numero "vacuna COVID" debe ser menor que 3',
+        ],
+        'IdUsuario' => [
+            'required' => 'El valor "ID usuario" es requerido',
+            'numeric' => 'El "ID usuario" debe ser un numero',
+            'is_valid_usuario' => 'el id usuario no existe'
+        ]
+    ];
 
-        public function estudiantes(){
-            $builder = $this->db->table($this->table.' e');
+    protected $skipValidation = false;
 
-            $builder->select('e.IdEstudiante, CONCAT(e.PrimerNombre, " ", e.SegundoNombre) Nombre');
-            $builder->select('CONCAT(e.PrimerApellido, " ", e.SegundoApellido) Apellido');
+    public function estudiantes()
+    {
+        $builder = $this->db->table($this->table . ' e');
 
-            $query = $builder->get();
+        $builder->select('e.IdEstudiante, CONCAT(e.PrimerNombre, " ", e.SegundoNombre) Nombre');
+        $builder->select('CONCAT(e.PrimerApellido, " ", e.SegundoApellido) Apellido');
 
-            return $query->getResult();
-        }
+        $query = $builder->get();
+
+        return $query->getResult();
     }
-?>
+}
